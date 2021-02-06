c5a5d8e1b9acbe402788a3e794fc0b9f76dd3c55
ricardo.loro <ricardo.loro.next@bbva.com>
2021-02-06 13:27:09 +0100

    feat(*): created mongo environment var

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDb = process.env.NODE_ENV === 'dev' ? 
 'mongodb://localhost:27017/cafe' :
-'mongodb+srv://cafe-user:123456_@cluster0.qrtll.mongodb.net/cafe';
+process.env.MONGO_URL;
 
 process.env.URLDB = urlDb;
